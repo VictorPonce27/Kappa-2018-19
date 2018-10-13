f08cc4cb6338cbf7b7758d43167c0d134d9837a8
Alaric <alaric.hunz@gmail.com>
2018-10-12 23:58:45 -0500

    Organized the code for tomorrow's competition

--- Rinzler v.1.c
+++ Rinzler v.1.c
@@ -40,173 +40,60 @@ void moveArm(float deg, int armSpeed);
 
 void pre_auton()
 {
-	resetMotorEncoder(Left);
-	resetMotorEncoder(Right);
-  bStopTasksBetweenModes = true;
 }
 
 task autonomous()
 {
-/*Last Year's Autonomous (In the Zone)
-
-	motor[LiftL]= 127;
-	motor[LiftR]= 127;
-	wait(1.0);
-	motor[LiftL]= 0;
-	motor[LiftR]= 0;
-//The lift moves up 1 second
-
-	motor[InstaL]= 127;
-	motor[InstaR]= 127;
-	wait(0.5);
-//Mobile goal goes forward
-	motor[InstaL]= 0;
-	motor[InstaR]= 0;
-//Mobile goal stops
-
-	resetMotorEncoder(Left);
-	resetMotorEncoder(Right);
-	while(abs(getMotorEncoder(Right)) < getStraightGoal(125))
-	{
-		motor[Left]= 85;
-		motor[Right]= 85;
-	}
-	motor[Left]= 0;
-	motor[Right]= 0;
-//Robot goes forward to collect the mobile goal
-
-	motor[LiftL]= -127;
-	motor[LiftR]= -127;
-	wait (1);
-//The lift moves down 1 second
- 	motor[LiftL]= 0;
-	motor[LiftR]= 0;
-//The lift stops
-
-	motor[Claw]= 127;
-	wait(1.5);
-	motor[LiftL]= 127;
-	motor[LiftR]= 127;
-	wait(1.5);
-//The lift moves up 1.5 seconds
- 	motor[LiftL]= 0;
-	motor[LiftR]= 0;
-	motor[Claw]= 0;
-//The lift stops
-
-	motor[InstaL]= -127;
-	motor[InstaR]= -127;
-	wait(0.5);
-//Mobile goal goes backward
-	motor[InstaL]=0;
-	motor[InstaR]=0;
-//Mobile goal stops
-
-	resetMotorEncoder(Left);
-	resetMotorEncoder(Right);
-	while(abs(getMotorEncoder(Right)) < getStraightGoal(122))
-	{
-		motor[Left]= -127;
-		motor[Right]= -127;
-	}
-	motor[Left]= 0;
-	motor[Right]= 0;
-// The robot goes  backward from collecting the mobile goal
-
-	resetMotorEncoder(Left);
-	resetMotorEncoder(Right);
-	while(abs(getMotorEncoder(Right)) < getRotationGoal(265))
-	{
-		motor[Left]= -127;
-		motor[Right]= 127;
-	}
-	motor[Left]= 0;
-	motor[Right]= 0;
-// The robot turns 265 degrees
-
-	resetMotorEncoder(Left);
-	resetMotorEncoder(Right);
-	while(abs(getMotorEncoder(Right)) < getStraightGoal(45))
-	{
-		motor[Left]= 127;
-		motor[Right]= 127;
-	}
-	motor[Left]= 0;
-	motor[Right]= 0;
-// Robot goes forward for a little bit
-
-	motor[InstaL]= 127;
-	motor[InstaR]= 127;
-	wait(0.5);
-//Mobile goal goes forward
-	motor[InstaL]= 0;
-	motor[InstaR]= 0;
-//Mobile goal stops
-
-	resetMotorEncoder(Left);
-	resetMotorEncoder(Right);
-	while(abs(getMotorEncoder(Right)) < getStraightGoal(50))
-	{
-		motor[Left]= -127;
-		motor[Right]= -127;
-	}
-	motor[Left]= 0;
-	motor[Right]= 0;
-// The robot backs away
-
-	motor[Claw]= 0;
-// Claw stops
-*/
 }
 
 task usercontrol()
 {
+	while (true)
+		{
+ 			int ClawM = vexRT(Ch2) ;
+ 			int LiftUp = vexRT(Btn5U);
+ 			int LiftDown = vexRT(Btn5D);
+ 			int LiftUp2 = vexRT(Btn6U);
+ 			int LiftDown2 = vexRT(Btn6D);
+ 			int leftstickX;
+ 			int leftstickY;
+ 			int deadzone = 5;
 
+ //////////////////Arcade Drive/////////////////
+	if (abs(vexRT[Ch3]) > deadzone)
+		{leftstickY = vexRT[Ch3];}
 
-while (true)
-	{
+	else
+		{leftstickY = 0;}
 
- int ClawM = vexRT(Ch2) ;
- int LiftUp =	vexRT(Btn5U);
- int LiftDown	= vexRT(Btn5D);
- int LiftUp2 = vexRT(Btn6U);
- int LiftDown2 = vexRT(Btn6D);
- int leftstickX;
- int leftstickY;
- int deadzone = 5;
+	if (abs(vexRT[Ch4]) > deadzone)
+		{leftstickX = vexRT[Ch4];}
 
- //////////////////Arcade drive/////////////////
- if (abs(vexRT[Ch3]) > deadzone){
-			leftstickY = vexRT[Ch3];
-		}
-		else {
-			leftstickY = 0;
-		}
+	else
+		{leftstickX =0 ;}
 
-		if (abs(vexRT[Ch4]) > deadzone){
-			leftstickX = vexRT[Ch4];
-		}
-		else {
-			leftstickX =0 ;
-		}
+	motor[Left] = leftstickY + leftstickX;
+	motor[Right]= leftstickY - leftstickX;
+/////////////////Arcade Drive///////////////////////
 
-		motor[Left] = leftstickY + leftstickX;
-		motor[Right]= leftstickY - leftstickX;
-/////////////////Arcade drive///////////////////////
-
-//////////////////Lift///////////////////////////////
-		if (LiftUp == 1) {
+//////////////////Lift//////////////////////////////
+	if (LiftUp == 1)
+		{
 			motor[LiftR] = 127;
 			motor[LiftL] = 127;
 		}
-	else if (LiftDown == 1){
+
+	else if (LiftDown == 1)
+		{
 			motor[LiftR] = -127;
 			motor[LiftL] = -127;
 		}
-			else{
-				motor[LiftR] = 0;
-				motor[LiftL] = 0;
-			}
+
+	else
+		{
+			motor[LiftR] = 0;
+			motor[LiftL] = 0;
+		}
 //////////////////Lift///////////////////////////////
 
 /////////////////Second Lift/////////////////////////
@@ -222,106 +109,27 @@ while (true)
 		}
 	else
 		{
-				motor[LiftR2] = 0;
-				motor[LiftL2] = 0;
+			motor[LiftR2] = 0;
+			motor[LiftL2] = 0;
 		}
+/////////////////Second Lift//////////////////////////
 
-//////////////////Claw////////////////////////////////
-				if (ClawM >= 5) {
-					motor [Claw] = ClawM;
-				}
-					else if (ClawM <= -5) {
-					motor [Claw] = ClawM;
-				}
-					else {
-					motor [Claw] = 0;
-				}
-//////////////////////////////////////////////////
-/*
-		int liftup=vexRT(Btn6U);
-		int liftdown=vexRT(Btn6D);
-		int clawopen=vexRT(Btn5U);
-		int clawclose=vexRT(Btn5D);
-		int mobileup=vexRT(Btn8D);
-		int mobiledown=vexRT(Btn8U);
-		int leftstickX;
-  	int leftstickY;
-  	int deadzone   = 5;
+//////////////////Claw///////////////////////////////
+	if (ClawM >= 5)
+		{motor [Claw] = ClawM;}
 
-/////////// Arcade Drive////////////////////////
-  	if(abs(vexRT[Ch3]) >deadzone)
-  	{leftstickY = vexRT[Ch3];}
+	else if (ClawM <= -5)
+		{motor [Claw] = ClawM;}
 
-		else
-		{leftstickY = 0;}
-
-		if(abs(vexRT[Ch4]) > deadzone)
-		{leftstickX = vexRT[Ch4];}
-
-		else
-		{leftstickX =0 ;}
-
-		motor[Left] = leftstickY + leftstickX;
-		motor[Right]= leftstickY - leftstickX;
-/////////// Arcade Drive///////////////////////
-
-/////////// Lift///////////////////////////////
-		if (liftup == 1)
-		{
-			motor [LiftR] = 127;
-			motor [LiftL] = 127;
-		}
-
-		else if (liftdown == 1)
-		{
-			motor [LiftL] = -127;
-			motor [LiftR] = -127;
-		}
-
-		else
-		{
-			motor [LiftR] = 0;
-			motor [LiftL] = 0;
-		}
-/////////// Lift///////////////////////////////
-
-/////////// Claw///////////////////////////////
-		if (clawopen == 1)
-	  	motor [Claw] = 127;
-
-	 	else if (clawclose == 1)
-	 	  motor [Claw] = -127;
-
-	 	else
-	 		{motor [Claw] = 0;}
-/////////// Claw///////////////////////////////
-
-/////////// Mobile Goal////////////////////////
-   	if (mobiledown == 1)
-   	{
-   		motor[InstaR] = 63.5;
-   		motor[InstaL] = 63.5;
-  	}
-
-  	else if (mobileup == 1)
-  	{
-  		motor[InstaR] = -63.5;
-  		motor[InstaL] = -63.5;
-  	}
-
-  	else
-  	{
-  		motor [InstaR] = 0;
-  		motor [InstaL] = 0;
-  	}
-/////////// Mobile Goal////////////////////////
-*/
+	else
+		{motor [Claw] = 0;}
+//////////////////Claw///////////////////////////////
 
     UserControlCodePlaceholderForTesting();
   }
 }
 
-/////////// Functions//////////////////////////
+///////////////Functions////////////////////////
 	int getStraightGoal(float cm)
 		{return 392*(cm/(wheelRadiusCM*M_PI));}
 
@@ -344,4 +152,4 @@ void moveArm(float deg, int armSpeed)
 			{motor[LiftR] = motor[LiftL] = -armSpeed;}
 		}
 }
-/////////// End of Functions///////////////////
+/////////////End of Functions///////////////////
